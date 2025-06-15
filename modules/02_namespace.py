da4c49d58c015397d3e7315af78b55e964178292
rmalldt <rmall.dt@gmail.com>
2025-06-15 23:38:55 +0100

    Update namespace comments

--- modules/02_namespace.py
+++ modules/02_namespace.py
@@ -5,17 +5,17 @@ import random
 
 x = 10  # global namespace
 print(f"Global: {x}")  # prints 10
-print(f"Global modules: {dir()}")  # lists all modules in global namespace
-print(f"Built-in modules: {dir(__builtins__)}")  # print
+print(f"Global modules: {dir()}")  # lists all names in current modules global namespace
+print(f"Built-in modules: {dir(__builtins__)}")  # print built-in modules
 
 
 def foo():
     x = 5  # local namespace
-    print(f"Local: {x}")  # built-in namespace
+    print(f"Local: {x}")  # prints 5
 
     def bar():
-        x = 0
-        print(f"Enclosing: {x}")
+        x = 0  # enclosing namespace
+        print(f"Enclosing: {x}")  # prints 0
 
     bar()
 
@@ -25,7 +25,9 @@ def func():
     b = 50
 
     res = a + b
-    print(f"Inside local function namespace is: {dir()}")
+    print(
+        f"Inside local function namespace is: {dir()}"
+    )  # list names in local namespace
     print(f"Locals: {locals()}")
 
 
