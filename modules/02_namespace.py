e3791119f7525937d2fb228b788510bb97093ccc
rmalldt <rmall.dt@gmail.com>
2025-06-16 23:45:20 +0100

    Update namespace with nonlocal and global use cases

--- modules/02_namespace.py
+++ modules/02_namespace.py
@@ -14,15 +14,13 @@ print(f"Built-in modules: {dir(__builtins__)}")  # print builtin modules
 def foo():
     x = 5  # local enclosing scope
     print(f"Enclosing local: {x}")  # prints 5
-    greeting = "hello"
 
     def bar():
-        x = 0  # local scope (it is also a free variable since it is already defined in enclosing scope)
+        x = 0  # local scope
         print(f"Nested local: {x}")  # prints 0
-        # inner function can access variables of enclosing function
-        print(f"Greeting: {greeting}")
 
     bar()
+    print(f"Enclosing local: {x}")
 
 
 def func():
@@ -35,10 +33,59 @@ def func():
     print(f"Locals: {locals()}")
 
 
+def free_variable():
+    greeting = "hello"
+
+    def innerfun():
+        # free variable, same name as enclosing variable but bound to new value
+        greeting = "Hi"
+        print(f"Inner greeting: {greeting}")  # prints Hi
+
+    innerfun()
+    print(f"Enclosing greeting: {greeting}")  # prints Hello
+
+
+def nonlocal_modify_enclosing_variable():
+    greeting = "hello"
+
+    def innerfun():
+        nonlocal greeting
+        greeting = "Hi"
+        print(f"Inner greeting: {greeting}")  # prints Hi
+
+    innerfun()
+    print(f"Enclosing greeting: {greeting}")  # prints Hi
+
+
+# Global variable
+result = 0
+
+
+def add(a, b):
+    result = a + b  # local scope
+
+
+add(1, 1)
+print(f"Result: {result}")  # prints 0
+
+
+def add_global(a, b):
+    global result  # using global keyword will update global variable here
+    result = a + b
+
+
+add_global(1, 1)
+print(f"Result: {result}")  # prints 2
+
+
 # ------------------ Test
 
-foo()
-
+# foo()
 # func()
+
+# free_variable()
+# nonlocal_modify_enclosing_variable()
+
+
 # print(f"Globals: {globals()}")
 # print(f"Global object item: {globals()['x']}")
