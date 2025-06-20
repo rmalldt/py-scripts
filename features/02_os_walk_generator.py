f2a88fda3146a30eed42014c58d7de25f15546ed
rmalldt <rmall.dt@gmail.com>
2025-06-20 17:03:56 +0100

    Add os.walk generator examples

--- features/02_os_walk_generator.py
+++ features/02_os_walk_generator.py
@@ -1,14 +1,60 @@
 import os
+import fnmatch
 
 
-def walk_dir(path: str):
-    for path, directories, files in os.walk(path, topdown=True):
+# os.walk is a generator so does not read every file at once into a huge list.
+# It only yields the details for a single directoru at a time.
+# So, we can process large dataset without running out of memory.
+def walk_dir(filepath: str):
+    for path, directories, files in os.walk(filepath, topdown=True):
         print(f"Path: {path}")
         print(f"Directories: {directories}")
         for f in files:
             print(f"\t{f}")
 
 
+def walk_dir_format(filepath: str):
+    for path, directories, files in os.walk(filepath, topdown=True):
+        if files:
+            print(f"Path: {path}")
+            first_split = os.path.split(path)
+            print(f"First split: {first_split}")
+            second_split = os.path.split(first_split[0])
+            print(f"Second split: {second_split}")
+
+            for f in files:
+                song_details = f[:-5].split(" - ")  # remove extension
+                print(song_details)
+            print("*" * 40)
+
+
+def find_albums(filepath, name):
+    for path, directories, files in os.walk(filepath, topdown=True):
+        for artist in fnmatch.filter(directories, name):
+            subdir = os.path.join(path, artist)
+            for album_path, albums, _ in os.walk(subdir):
+                for album in albums:
+                    yield os.path.join(album_path, album), album  # yield path and album
+
+
+def list_songs(albums):
+    for album in albums:
+        for song in os.listdir(album[0]):  # get path, not the album name
+            yield song
+
+
 # ------------------ Test
+
+
 dir_path = "data/music"
-walk_dir(dir_path)
+
+# walk_dir(dir_path)
+# walk_dir_format(dir_path)
+
+album_list = find_albums(dir_path, "Black*")
+# for a in album_list:
+#     print(a)
+
+song_list = list_songs(album_list)
+for a in song_list:
+    print(a)
